d0952b04e04accf8bc41897c37c6275d4cb19a48
TayArmy <111232494+TayArmy@users.noreply.github.com>
2023-03-19 06:52:53 +0100

    v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -29,7 +29,8 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
-    Args:
+   
+   Args:
         archive_path (str): The path of the archive to distribute.
     Returns:
         If the file doesn't exist at archive_path or an error occurs - False.
